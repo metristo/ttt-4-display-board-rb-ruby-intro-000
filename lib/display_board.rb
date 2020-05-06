634966b69a8b5d733ac34dcbe3c13177f83c6bfc
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-06 12:55:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+grid = ["   ", " X ", " O "]
+def display_board(1)
+  puts "#{grid(0)}|#{grid(0)}|#{grid(0)}"
+  puts "-----------"
+  puts "#{grid(0)}|#{grid(0)}|#{grid(0)}"
+  puts "-----------"
+  puts "#{grid(0)}|#{grid(0)}|#{grid(0)}"
+end
+def display_board(2)
+  puts "#{grid(0)}|#{grid(0)}|#{grid(0)}"
+  puts "-----------"
+  puts "#{grid(0)}|#{grid(1)}|#{grid(0)}"
+  puts "-----------"
+  puts "#{grid(0)}|#{grid(0)}|#{grid(0)}"
+end
